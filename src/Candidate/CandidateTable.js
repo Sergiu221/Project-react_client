b7caa1e05bf85b6ace2e59fc4e5124375d5c1fa0
Sergiu221 <sergiu221@gmail.com>
2019-01-09 07:02:45 +0100

    Save work

---
+++ src/Candidate/CandidateTable.js
@@ -0,0 +1,25 @@
+import React, {
+  Component
+}
+from 'react';
+import ReactDOM from 'react-dom';
+
+class CandidateTable extends Component{
+
+  constructor(props) {
+    super(props);
+    console.log("Incarcare pagina");
+    this.state = ({
+
+    });
+
+  }
+
+    render() {
+      return (
+        <h1> Am nevoie de inspiratie</h1>
+      );
+    }
+  }
+
+  export default CandidateTable;

2d85890dc41d1389cf101dda5b42f47f14e9d765
Sergiu221 <sergiu221@gmail.com>
2019-01-19 11:30:55 +0100

    feat : PROIECT-38 supervisors table

--- src/supervisors/SupervisorsTable.js
+++ src/supervisors/SupervisorsTable.js
@@ -8,6 +8,7 @@ class SupervisorsTable extends Component {
     this.state = {
       supervisors: []
     };
+    this.renderEditable = this.renderEditable.bind(this);
   }
 
   componentDidMount() {
@@ -22,16 +23,39 @@ class SupervisorsTable extends Component {
         });
       });
   }
+
+  renderEditable(cellInfo) {
+    return (
+      <div
+        style={{ backgroundColor: "#fafafa" }}
+        contentEditable
+        suppressContentEditableWarning
+        onBlur={e => {
+          const data = [...this.state.supervisors];
+          data[cellInfo.index][cellInfo.column.id] = e.target.innerHTML;
+          this.updateSupervisor(cellInfo.original.id, cellInfo.original);
+          this.setState({ data });
+          console.log(cellInfo.original);
+        }}
+        dangerouslySetInnerHTML={{
+          __html: this.state.supervisors[cellInfo.index][cellInfo.column.id]
+        }}
+      />
+    );
+  }
+
   render() {
     const columns = [
       {
         Header: "Nume",
-        accessor: "firstName"
+        accessor: "firstName",
+        Cell: this.renderEditable
       },
       {
         Header: "Prenume",
-        accessor: "lastName"
-      },
+        accessor: "lastName",
+        Cell: this.renderEditable
+      }
     ];
     return (
       <ReactTable
@@ -40,6 +64,22 @@ class SupervisorsTable extends Component {
         noDataText={"Te rog asteapta"}
       />
     );
+  }
+
+  updateSupervisor(id, data) {
+    return fetch("http://localhost:8080/supervisors/" + id, {
+      method: "PUT",
+      body: JSON.stringify(data),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+      .then(res => {
+        return res;
+      })
+      .catch(err => {
+        console.log(err);
+      });
   }
 }
 export default SupervisorsTable;

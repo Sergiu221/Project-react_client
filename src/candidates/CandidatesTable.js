2e7d48271b4efffd0d9886d7e139c0c4117862ca
Sergiu221 <sergiu221@gmail.com>
2019-01-19 11:21:10 +0100

    feat : PROIECT-38 candidates table

--- src/candidates/CandidatesTable.js
+++ src/candidates/CandidatesTable.js
@@ -8,6 +8,7 @@ class CandidatesTable extends Component {
     this.state = {
       candidates: []
     };
+    this.renderEditable = this.renderEditable.bind(this);
   }
 
   componentDidMount() {
@@ -22,32 +23,72 @@ class CandidatesTable extends Component {
         });
       });
   }
+
+  renderEditable(cellInfo) {
+    return (
+      <div
+        style={{ backgroundColor: "#fafafa" }}
+        contentEditable
+        suppressContentEditableWarning
+        onBlur={e => {
+          const data = [...this.state.candidates];
+          data[cellInfo.index][cellInfo.column.id] = e.target.innerHTML;
+          this.updateCandidate(cellInfo.original.id, cellInfo.original);
+          this.setState({ data });
+          console.log(cellInfo.original);
+        }}
+        dangerouslySetInnerHTML={{
+          __html: this.state.candidates[cellInfo.index][cellInfo.column.id]
+        }}
+      />
+    );
+  }
   render() {
     const columns = [
       {
         Header: "CNP",
-        accessor: "cnp"
+        accessor: "cnp",
+        Cell: this.renderEditable
       },
       {
         Header: "Nume",
-        accessor: "firstName"
+        accessor: "firstName",
+        Cell: this.renderEditable
       },
       {
         Header: "Prenume",
-        accessor: "lastName"
+        accessor: "lastName",
+        Cell: this.renderEditable
       },
       {
         Header: "Liceu",
-        accessor: "liceu"
+        accessor: "liceu",
+        Cell: this.renderEditable
       }
     ];
     return (
       <ReactTable
         columns={columns}
         data={this.state.candidates}
-        noDataText={"Te rog asteapta"}
+        noDataText={"Te rog, asteapta"}
       />
     );
+  }
+
+  updateCandidate(id, data) {
+    return fetch("http://localhost:8080/candidates/" + id, {
+      method: "PUT",
+      body: JSON.stringify(data),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+      .then(res => {
+        return res;
+      })
+      .catch(err => {
+        console.log(err);
+      });
   }
 }
 export default CandidatesTable;

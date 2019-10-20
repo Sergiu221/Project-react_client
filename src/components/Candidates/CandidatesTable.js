1b8a4f1cc0501702619b450f09cd46b57beaffd3
Sergiu221 <sergiu221@gmail.com>
2019-10-20 22:24:17 +0200

    change view on candidate list

--- src/components/Candidates/CandidatesTable.js
+++ src/components/Candidates/CandidatesTable.js
@@ -1,99 +1,92 @@
-import React, { Component } from "react";
-import ReactTable from "react-table";
-import "react-table/react-table.css";
-import ReactExport from "react-data-export";
-import CandidateForm from "./CandidateForm.js";
-import {
-  getHeaderTable,
-  getHeaderTableFilter
-} from "./UtilCandidatesFunctions.js";
+import React, {Component} from "react";
+import {BootstrapTable, TableHeaderColumn} from "react-bootstrap-table";
+import "react-bootstrap-table/dist/react-bootstrap-table-all.min.css";
+import styled from "styled-components";
+
+
+const Styles = styled.div`
+.btn {
+    text-transform: uppercase;
+    letter-spacing: 2px; 
+    }`
+;
+
 class CandidatesTable extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      candidates: []
-    };
-    this.renderEditable = this.renderEditable.bind(this);
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            candidates: []
+        };
+    }
 
-  componentDidMount() {
-    const url = "http://localhost:8080/candidates";
-    fetch(url, {
-      method: "GET"
-    })
-      .then(response => response.json())
-      .then(candidates => {
-        this.setState({
-          candidates: candidates
-        });
-      });
-  }
+    componentDidMount() {
+        const url = "http://localhost:8080/candidates";
+        fetch(url, {
+            method: "GET"
+        })
+            .then(response => response.json())
+            .then(candidates => {
+                this.setState({
+                    candidates: candidates
+                });
+            });
+    }
 
-  renderEditable(cellInfo) {
-    return (
-      <div
-        style={{ backgroundColor: "#fafafa" }}
-        contentEditable
-        suppressContentEditableWarning
-        onBlur={e => {
-          const data = [...this.state.candidates];
-          data[cellInfo.index][cellInfo.column.id] = e.target.innerHTML;
-          this.updateCandidate(cellInfo.original.id, cellInfo.original);
-          this.setState({ data });
-          console.log(cellInfo.original);
-        }}
-        dangerouslySetInnerHTML={{
-          __html: this.state.candidates[cellInfo.index][cellInfo.column.id]
-        }}
-      />
-    );
-  }
+    render() {
+        console.log(this.state.candidates);
+        return (
+            <Styles>
+                <BootstrapTable data={this.state.candidates}
+                                options={options}
+                                insertRow={true}
+                                deleteRow={true}
+                                search={true}
+                                multiColumnSearch={true}
+                                exportCSV={true}
+                                selectRow={selectRowProp}
+                                cellEdit={cellEditProp}>
+                    <TableHeaderColumn dataField='id' isKey hidden searchable={false}>ID</TableHeaderColumn>
+                    <TableHeaderColumn dataField='cnp' dataSort={true}>CNP</TableHeaderColumn>
+                    <TableHeaderColumn dataField='firstName'>Prenume</TableHeaderColumn>
+                    <TableHeaderColumn dataField='lastName' dataSort={true}>Nume</TableHeaderColumn>
+                    <TableHeaderColumn dataField='highSchool'>Liceu</TableHeaderColumn>
+                </BootstrapTable>
+            </Styles>
+        );
+    }
 
-  render() {
-    const ExcelFile = ReactExport.ExcelFile;
-    const ExcelSheet = ReactExport.ExcelFile.ExcelSheet;
-    const ExcelColumn = ReactExport.ExcelFile.ExcelColumn;
-    return (
-      <div>
-        <CandidateForm />
-        <ReactTable
-          columns={getHeaderTable(this.renderEditable)}
-          data={this.state.candidates}
-          noDataText={"Te rog, asteapta"}
-        />
-        <ExcelFile filename="Candidati">
-          <ExcelSheet data={this.state.candidates} name="Candidates">
-            {getHeaderTableFilter(this.renderEditable).map(function(
-              ColumnHeader
-            ) {
-              return (
-                <ExcelColumn
-                  key={ColumnHeader.Header}
-                  label={ColumnHeader.Header}
-                  value={ColumnHeader.accessor}
-                />
-              );
-            })}
-          </ExcelSheet>
-        </ExcelFile>
-      </div>
-    );
-  }
-
-  updateCandidate(id, data) {
-    return fetch("http://localhost:8080/candidates/" + id, {
-      method: "PUT",
-      body: JSON.stringify(data),
-      headers: {
-        "Content-Type": "application/json"
-      }
-    })
-      .then(res => {
-        return res;
-      })
-      .catch(err => {
-        console.log(err);
-      });
-  }
+    updateCandidate(id, data) {
+        return fetch("http://localhost:8080/candidates/" + id, {
+            method: "PUT",
+            body: JSON.stringify(data),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+            .then(res => {
+                return res;
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    }
 }
+
+const cellEditProp = {
+    mode: 'click'
+};
+// If you want to enable deleteRow, you must enable row selection also.
+const selectRowProp = {
+    mode: 'checkbox'
+};
+
+const options = {
+    exportCSVText: 'my_export',
+    insertText: 'my_insert',
+    deleteText: 'my_delete',
+    saveText: 'my_save',
+    closeText: 'my_close'
+};
+
+
 export default CandidatesTable;

eb6b963c8711936426f49b73c5af291dc0f09044
Sergiu221 <sergiu221@gmail.com>
2019-11-07 08:12:55 +0100

    Move asside table options

---
+++ src/components/table-options.js
@@ -0,0 +1,9 @@
+export default function tableOptions() {
+    return {
+        exportCSVText: 'descarca',
+        insertText: 'inseriaza',
+        deleteText: 'sterge',
+        saveText: 'salveaza',
+        closeText: 'inchide'
+    };
+}

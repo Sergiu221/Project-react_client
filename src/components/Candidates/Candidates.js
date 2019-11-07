31ead7c172d3f7d735291d77f0dd0b7a48727d82
Sergiu221 <sergiu221@gmail.com>
2019-11-07 08:13:26 +0100

    Update Candidates with new restructure

--- src/components/Candidates/Candidates.js
+++ src/components/Candidates/Candidates.js
@@ -1,4 +1,6 @@
-import React, {Component} from "react";
+import React, {useState, useEffect} from "react";
+import tableOptions from "../table-options";
+import API from "../utils/API";
 import {BootstrapTable, TableHeaderColumn} from "react-bootstrap-table";
 import "react-bootstrap-table/dist/react-bootstrap-table-all.min.css";
 import styled from "styled-components";
@@ -11,89 +13,55 @@ const Styles = styled.div`
     }`
 ;
 
-class Candidates extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            candidates: []
-        };
-    }
+export default function Candidates(props) {
+    const [candidates, setCandidates] = useState([]);
+    const [option] = useState(tableOptions);
 
-    componentDidMount() {
-        const url = api + "/candidates";
-        console.log(url);
-        fetch(url, {
-            method: "GET"
-        })
-            .then(response => response.json())
-            .then(candidates => {
-                this.setState({
-                    candidates: candidates
-                });
-            });
-    }
+    useEffect(() => {
+        (async () => {
+            const result = await API.get('/candidates');
+            console.log(result.data);
+            setCandidates(result.data);
+        })();
+    }, []);
 
-    render() {
-        console.log(this.state.candidates);
-        return (
-            <Styles>
-                <BootstrapTable data={this.state.candidates}
-                                options={options}
-                                insertRow={true}
-                                deleteRow={true}
-                                search={true}
-                                multiColumnSearch={true}
-                                exportCSV={true}
-                                selectRow={selectRowProp}
-                                cellEdit={cellEditProp}>
-                    <TableHeaderColumn dataField='id' isKey hidden searchable={false}>ID</TableHeaderColumn>
-                    <TableHeaderColumn dataField='cnp' dataSort={true}>CNP</TableHeaderColumn>
-                    <TableHeaderColumn dataField='firstName'>Prenume</TableHeaderColumn>
-                    <TableHeaderColumn dataField='lastName' dataSort={true}>Nume</TableHeaderColumn>
-                    <TableHeaderColumn dataField='highSchool'>Liceu</TableHeaderColumn>
-                </BootstrapTable>
-            </Styles>
-        );
-    }
-}
-
-function onBeforeSaveCell(row, cellName, cellValue) {
-    console.log("Change " + cellName + "to value: " + cellValue + " with row-id:" + row.id);
-    row[cellName] = cellValue;
-    return fetch(api + "/candidates/" + row.id, {
-        method: "PUT",
-        body: JSON.stringify(row),
-        headers: {
-            "Content-Type": "application/json"
-        }
-    })
-        .then(res => {
-            return res;
-        })
-        .catch(err => {
-            console.log(err);
+    function onBeforeSaveCell(row, cellName, cellValue) {
+        console.log("Change " + cellName + "to value: " + cellValue + " with row-id:" + row.id);
+        row[cellName] = cellValue;
+        API.put("/candidates/" + row.id, row).then((response) => {
+            console.log(response);
+        }, (error) => {
+            console.log(error);
         });
+    }
 
+    const cellEditProp = {
+        mode: 'click',
+        blurToSave: true,
+        beforeSaveCell: onBeforeSaveCell
+    };
+
+    const selectRowProp = {
+        mode: 'checkbox'
+    };
+
+    return (
+        <Styles>
+            <BootstrapTable data={candidates} //sa vina de pe props....
+                            options={option}
+                            insertRow={true}
+                            deleteRow={true}
+                            search={true}
+                            multiColumnSearch={true}
+                            exportCSV={true}
+                            selectRow={selectRowProp}
+                            cellEdit={cellEditProp}>
+                <TableHeaderColumn dataField='id' isKey hidden searchable={false}>ID</TableHeaderColumn>
+                <TableHeaderColumn dataField='cnp' dataSort={true}>CNP</TableHeaderColumn>
+                <TableHeaderColumn dataField='firstName'>Prenume</TableHeaderColumn>
+                <TableHeaderColumn dataField='lastName' dataSort={true}>Nume</TableHeaderColumn>
+                <TableHeaderColumn dataField='highSchool'>Liceu</TableHeaderColumn>
+            </BootstrapTable>
+        </Styles>
+    );
 }
-
-const cellEditProp = {
-    mode: 'click',
-    blurToSave: true,
-    beforeSaveCell: onBeforeSaveCell
-};
-// If you want to enable deleteRow, you must enable row selection also.
-const selectRowProp = {
-    mode: 'checkbox'
-};
-
-const options = {
-    exportCSVText: 'my_export',
-    insertText: 'my_insert',
-    deleteText: 'my_delete',
-    saveText: 'my_save',
-    closeText: 'my_close'
-};
-
-const api = process.env.REACT_APP_API_HOST;
-
-export default Candidates;

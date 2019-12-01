293730ae2f7645b453e58a2df8a7e6afaa98ff9a
Sergiu221 <sergiu221@gmail.com>
2019-12-01 13:04:48 +0100

    Use REACT_APP_STAGE to specify enviroment

--- src/components/utils/API.js
+++ src/components/utils/API.js
@@ -7,8 +7,8 @@ export default axios.create({
 });
 
 function getBaseURL() {
-    if (process.env.NODE_ENV !== 'production') {
-        console.log("We are on [" + process.env.NODE_ENV + "] evoiroment");
+    if (process.env.REACT_APP_STAGE !== 'production') {
+        console.log("REACT_APP_STAGE:" + process.env.REACT_APP_STAGE);
         return 'http://localhost:8080';
     }
     return 'https://api-licenta.herokuapp.com';

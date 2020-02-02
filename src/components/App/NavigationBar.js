d487e7b029eed2046261ec05273aecca4c40db48
Sergiu221 <sergiu221@gmail.com>
2020-02-02 18:57:35 +0100

    Change style for NavigationBar and use active link.

--- src/components/App/NavigationBar.js
+++ src/components/App/NavigationBar.js
@@ -4,21 +4,8 @@ import styled from 'styled-components';
 import {MyContext} from "../MyContext";
 import {Redirect} from "react-router-dom";
 import {authService} from "../utils/API";
-
-const Styles = styled.div`
-  .navbar-toggler-icon{
-    background-color: #bbb
-  }
-  .navbar {
-    background-color: #222;
-  }
-  a, .navbar-brand, .navbar-nav .nav-link {
-    color: #bbb;
-    &:hover {
-      color: white;
-    }
-  }
-`;
+import NavLink from "react-router-dom/NavLink";
+import "./NavigationBar.css"
 
 export default function NavigationBar() {
     const [redirect, toggleRedirect] = useState(false);
@@ -29,32 +16,32 @@ export default function NavigationBar() {
     };
 
     return (
-        <Styles>
-            <Navbar expand="lg">
+        <React.Fragment>
+            <Navbar expand="lg" className="nav nav-tabs" id="nav-tab" role="tablist">
                 <Navbar.Toggle aria-controls="basic-navbar-nav"/>
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="mr-auto">
-                        <Nav.Item><Nav.Link href="/home"><h5>Home</h5></Nav.Link></Nav.Item>
-                        <Nav.Item><Nav.Link href="/categories"><h5>Categorii</h5></Nav.Link></Nav.Item>
-                        <Nav.Item><Nav.Link href="/candidates"><h5>Candidati</h5></Nav.Link></Nav.Item>
-                        <Nav.Item><Nav.Link href="/candidates_options"><h5>Candidati Optiuni</h5></Nav.Link></Nav.Item>
-                        <Nav.Item><Nav.Link href="/supervisors"><h5>Supraveghetori</h5></Nav.Link></Nav.Item>
-                        <Nav.Item><Nav.Link href="/halls"><h5>Sali</h5></Nav.Link></Nav.Item>
-                        <Nav.Item><Nav.Link href="/grades"><h5>Note</h5></Nav.Link></Nav.Item>
-                        <MyContext.Consumer>
-                            {(context) => {
-                                if (context.state.cool === "show") {
-                                    return (<Nav.Item> <Nav.Link href="/reports"><h5>Rapoarte</h5>
-                                    </Nav.Link></Nav.Item>)
-                                }
-                            }}
-                        </MyContext.Consumer>
-
+                        <NavLink to="/home" className={'main-bar'} activeClassName={'main-nav-active'}>
+                            <h5>Home</h5></NavLink>
+                        <NavLink to="/categories" className={'main-bar'} activeClassName={'main-nav-active'}>
+                            <h5>Categorii</h5></NavLink>
+                        <NavLink to="/candidates" className={'main-bar'} activeClassName={'main-nav-active'}>
+                            <h5>Candidati</h5></NavLink>
+                        <NavLink to="/candidates_options" className={'main-bar'} activeClassName={'main-nav-active'}>
+                            <h5>Candidati Optiuni</h5></NavLink>
+                        <NavLink to="/supervisors" className={'main-bar'} activeClassName={'main-nav-active'}>
+                            <h5>Supraveghetori</h5></NavLink>
+                        <NavLink to="/halls" className={'main-bar'} activeClassName={'main-nav-active'}>
+                            <h5>Sali</h5></NavLink>
+                        <NavLink to="/grades" className={'main-bar'} activeClassName={'main-nav-active'}>
+                            <h5>Note</h5></NavLink>
+                        <NavLink to="/reports" className={'main-bar'} activeClassName={'main-nav-active'}>
+                            <h5>Rapoarte</h5></NavLink>
                     </Nav>
                     <Button variant="light" onClick={handleLogout}>Logout</Button>
                 </Navbar.Collapse>
             </Navbar>
             {redirect && <Redirect to='/login'/>}
-        </Styles>
+        </React.Fragment>
     );
 }
